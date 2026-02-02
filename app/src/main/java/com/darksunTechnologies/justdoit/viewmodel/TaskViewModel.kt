4ebe4a403d75c31f23bdda2f28a64333abcf0553
aaftabv22 <aaftabvhora62@gmail.com>
2026-02-02 16:42:04 -0500

    Upgrade AGP and Kotlin versions

--- app/src/main/java/com/darksunTechnologies/justdoit/viewmodel/TaskViewModel.kt
+++ app/src/main/java/com/darksunTechnologies/justdoit/viewmodel/TaskViewModel.kt
@@ -1,4 +1,24 @@
 package com.darksunTechnologies.justdoit.viewmodel
 
-class TaskViewModel {
+import androidx.lifecycle.ViewModel
+import com.darksunTechnologies.justdoit.models.Task
+
+class TaskViewModel : ViewModel() {
+    val taskList: MutableList<Task> = mutableListOf(
+        Task("Start with your first task from VM", false),
+        Task("Is it high priority task? turn of the switch on then from VM", true)
+    )
+
+    fun addTask(task: Task){
+        taskList.add(task)
+    }
+
+    fun removeTaskAt(index: Int){
+        taskList.removeAt(index)
+    }
+
+    fun clearAll(){
+        taskList.clear()
+    }
+
 }
